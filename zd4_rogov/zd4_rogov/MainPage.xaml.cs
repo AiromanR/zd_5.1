6a3da0fe04953509b988c1561d85f6cb305bb7c8
AiromanR <andrey_rogov_06@mail.ru>
2025-06-16 07:00:49 +0500

    zd5_1

---
+++ zd4_rogov/zd4_rogov/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.StyleSheets;
+
+namespace zd4_rogov
+{
+    public partial class MainPage : TabbedPage
+    {
+        public MainPage(string username)
+        {
+            InitializeComponent();
+            BarBackgroundColor = Color.Blue;
+            Title = $"Привет, {username}!";
+            this.Resources.Add(StyleSheet.FromResource("styles.css", IntrospectionExtensions.GetTypeInfo(typeof(MainPage)).Assembly));
+        }
+    }
+}
